e7fdbfe0b4ff49dc587f95b2cedc414242753ff0
heerucan <iwpow0307@gmail.com>
2022-04-24 19:26:22 +0900

    [Chore] public 접근제한자 추가

--- Sources/WALKit/Color/WALColor.swift
+++ Sources/WALKit/Color/WALColor.swift
@@ -13,18 +13,18 @@ import UIKit
 
 public enum WALColor {
     // Base Color
-    static let black = UIColor(named: "black100")
-    static let white = UIColor(named: "white100")
+    public static let black = UIColor(named: "black100")
+    public static let white = UIColor(named: "white100")
     
     // Gray Scale
-    static let gray100 = UIColor(named: "gray100")
-    static let gray200 = UIColor(named: "gray200")
-    static let gray300 = UIColor(named: "gray300")
-    static let gray400 = UIColor(named: "gray400")
-    static let gray500 = UIColor(named: "gray500")
-    static let gray600 = UIColor(named: "gray600")
+    public static let gray100 = UIColor(named: "gray100")
+    public static let gray200 = UIColor(named: "gray200")
+    public static let gray300 = UIColor(named: "gray300")
+    public static let gray400 = UIColor(named: "gray400")
+    public static let gray500 = UIColor(named: "gray500")
+    public static let gray600 = UIColor(named: "gray600")
     
     // Brand Color
-    static let orange100 = UIColor(named: "orange100")
-    static let mint100 = UIColor(named: "mint100")
+    public static let orange100 = UIColor(named: "orange100")
+    public static let mint100 = UIColor(named: "mint100")
 }
