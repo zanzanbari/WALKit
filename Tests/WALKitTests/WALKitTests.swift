487b93c84d134d72a5ea35f76955ddc8e2147fea
heerucan <iwpow0307@gmail.com>
2022-04-24 19:51:03 +0900

    [Add] TestCode 추가

--- Tests/WALKitTests/WALKitTests.swift
+++ Tests/WALKitTests/WALKitTests.swift
@@ -5,4 +5,25 @@ final class WALKitTests: XCTestCase {
     func testExample() throws {
         
     }
+    
+    func testLoadUIColorFromBundle() {
+        for color in UIColor.myProjectUIColors {
+            XCTAssertNotNil(color)
+        }
+    }
+}
+
+fileprivate extension UIColor {
+    static var myProjectUIColors: [UIColor] = [
+        UIColor.black100,
+        UIColor.white100, //It fails when it tries to access this color.
+        UIColor.gray100,
+        UIColor.gray200,
+        UIColor.gray300,
+        UIColor.gray400,
+        UIColor.gray500,
+        UIColor.gray600,
+        UIColor.orange100,
+        UIColor.mint100
+        ]
 }
