b8f954815a53608a21beb0e12f23cc8da7289902
heerucan <iwpow0307@gmail.com>
2022-04-25 14:53:26 +0900

    [Chore] 코드 변경

--- Sources/WALKit/Icon/WALIcon.swift
+++ Sources/WALKit/Icon/WALIcon.swift
@@ -7,20 +7,21 @@
 
 import UIKit
 
+public extension UIImage {
+    static let btnDelete = UIImage(named: "btn_delete")
+}
+
+//public extension WALIcon {
+//    var image: UIImage {
+//        guard let image = UIImage(named: self.rawValue) else {
+//            return UIImage()
+//        }
+//        return image
+//    }
+//}
 //
 //public extension UIImage {
 //  convenience init(asset: WALIcon) {
 //    self.init(named: asset.rawValue)!
 //  }
 //}
-
-public extension UIImage {
-    
-    enum WALIcon: String {
-        case btnDelete = "btn_delete"
-        
-        static func image(_ type: WALIcon) -> UIImage {
-            return UIImage(named: type.rawValue)!
-        }
-    }
-}
