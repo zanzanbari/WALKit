02ff2372af2c999aefa6357c7a9b2bd2a91acbe8
heerucan <jwyandkrh@naver.com>
2022-10-16 15:54:41 +0900

    [Chore] TextField RightViewMode 삭제

--- Sources/WALKit/Component/TextField/UITextField+Extension.swift
+++ Sources/WALKit/Component/TextField/UITextField+Extension.swift
@@ -10,11 +10,7 @@ import UIKit
 public extension UITextField {
     func setupPadding() {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: self.frame.height))
-        
         self.leftView = paddingView
         self.leftViewMode = ViewMode.always
-        
-        self.rightView = paddingView
-        self.rightViewMode = ViewMode.always
     }
 }
